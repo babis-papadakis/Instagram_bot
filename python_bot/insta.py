cdb3b37953b4cb7224e5eae36dc1c8f2d4554b36
Charalampos Papadakis <babis.papadakis2@gmail.com>
2020-10-23 16:52:35 +0300

    Update insta.py

--- python_bot/insta.py
+++ python_bot/insta.py
@@ -1,6 +1,6 @@
 from instapy import InstaPy
 
-session = InstaPy(username="babis.papadakis1", password="Papadakis1**")
+session = InstaPy(username="username", password="password")
 session.login()
 session.like_by_tags(["fashion", "style"], amount=5)
 session.set_dont_like(["naked", "nsfw"])
